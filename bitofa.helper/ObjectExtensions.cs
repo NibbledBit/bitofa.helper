347100e20f367e1107c6f16c2c9e69198c713dc3
Nibbles <me@nibbledbit.co.uk>
2022-01-12 13:37:13 +0000

    Initial Commit

---
+++ bitofa.helper/ObjectExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace bitofa.helper {
+    public static class ObjectExtensions {
+        public static bool Exists<T>(this T obj) {
+            return obj != null;
+        }
+        public static bool DoesNotExist<T>(this T obj) {
+            return obj == null;
+        }
+    }
+}
