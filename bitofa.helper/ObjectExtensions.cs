17e3e755a97291c83a2f9962759ba03e20f4f613
Nibbles <me@nibbledbit.co.uk>
2022-01-12 13:47:11 +0000

    teset

--- bitofa.helper/ObjectExtensions.cs
+++ bitofa.helper/ObjectExtensions.cs
@@ -4,6 +4,7 @@ using System.Text;
 
 namespace bitofa.helper {
     public static class ObjectExtensions {
+        //Test Comment to build auto
         public static bool Exists<T>(this T obj) {
             return obj != null;
         }
