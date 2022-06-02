0795aaf462f2227ed182284302f9b52ced78a6fc
Nibbles <me@nibbledbit.co.uk>
2022-06-02 13:12:57 +0100

    domain base

---
+++ bitofa.helper/DDD/DomainBase.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace BitOfA.Helper.DDD
+{
+    public abstract class DomainBase : IDomainModel
+    {
+
+        public DomainBase()
+        {
+            _domainEvents = new List<INotification>();
+        }
+
+        private List<INotification> _domainEvents;
+        public List<INotification> DomainEvents => _domainEvents;
+
+        public void AddDomainEvent(INotification eventItem)
+        {
+            _domainEvents.Add(eventItem);
+        }
+
+        public void RemoveDomainEvent(INotification eventItem)
+        {
+            _domainEvents?.Remove(eventItem);
+        }
+
+        public virtual int Id { get; protected set; }
+    }
+}
