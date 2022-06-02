41d09cff2370564bfeb5e5136c693f7ddb6feff7
Nibbles <me@nibbledbit.co.uk>
2022-06-02 13:37:08 +0100

    dpoain context version up

---
+++ bitofa.helper/Persistence/DomainContext.cs
@@ -0,0 +1,25 @@
+﻿using BitOfA.Helper.DDD;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BitOfA.Helper.Persistence
+{
+    public abstract class DomainContext : DbContext
+    {
+        private readonly DbContextOptions<DbContext> options;
+        private readonly INotifyDispatcher dispatcher;
+
+        public DomainContext(DbContextOptions<DbContext> options, INotifyDispatcher dispatcher)
+        {
+            this.options = options;
+            this.dispatcher = dispatcher;
+        }
+
+
+    }
+}
