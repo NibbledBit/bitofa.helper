7e3db2391d2f73d78f26a7810560978680f85d85
Nibbles <me@nibbledbit.co.uk>
2022-06-03 11:35:52 +0100

    Added IView back again

---
+++ bitofa.helper/MVVM/IView.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitOfA.Helper.MVVM
+{
+    public interface IView<T> where T : IViewModel
+    {
+        IViewModel ViewModel { get; }
+
+    }
+}
