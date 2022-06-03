ca056e64b1cb1a2504a6b09e8cd32e5a87b3ce7c
Nibbles <me@nibbledbit.co.uk>
2022-06-03 11:40:12 +0100

    Added viewmodel lifecycle and version up

---
+++ bitofa.helper/MVVM/ViewModelBase.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+
+namespace BitOfA.Helper.MVVM
+{
+    public abstract class ViewModelBase : IViewModel
+    {
+        public abstract Task OnAppearing();
+        public abstract Task OnDisappearing();
+        public abstract Task OnAppeared();
+        public abstract Task OnDispeared();
+    }
+}
