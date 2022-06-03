860c2d8dcc1a85de77388ab8713c6fb692f92d6b
Nibbles <me@nibbledbit.co.uk>
2022-06-03 16:09:56 +0100

    added random string

--- bitofa.helper/Extensions/StringExtensions.cs
+++ bitofa.helper/Extensions/StringExtensions.cs
@@ -1,38 +1,46 @@
 ﻿using System;
+using System.Linq;
 
-namespace BitOfA.Helper.Extensions
+namespace BitOfA.Helper.Extensions;
+
+public static class StringExtensions
 {
-    public static class StringExtensions
+    public static bool IsANumber(this string str)
     {
-        public static bool IsANumber(this string str)
-        {
-            return int.TryParse(str, out _);
-        }
-        public static string ReplaceLastOccurrence(this string source, string find, string replace)
-        {
-            int place = source.LastIndexOf(find);
-            return source.Remove(place, find.Length).Insert(place, replace);
-        }
-        /// <summary>
-        /// Truncates string so that it is no longer than the specified number of characters.
-        /// </summary>
-        /// <param name="str">String to truncate.</param>
-        /// <param name="length">Maximum string length.</param>
-        /// <returns>Original string or a truncated one if the original was too long.</returns>
-        public static string Truncate(this string str, int length)
-        {
-            if (length < 0)
-            {
-                throw new ArgumentOutOfRangeException(nameof(length), "Length must be >= 0");
-            }
-
-            if (str == null)
-            {
-                return null;
-            }
-
-            int maxLength = Math.Min(str.Length, length);
-            return str.Substring(0, maxLength);
-        }
+        return int.TryParse(str, out _);
     }
+    public static string ReplaceLastOccurrence(this string source, string find, string replace)
+    {
+        int place = source.LastIndexOf(find);
+        return source.Remove(place, find.Length).Insert(place, replace);
+    }
+    /// <summary>
+    /// Truncates string so that it is no longer than the specified number of characters.
+    /// </summary>
+    /// <param name="str">String to truncate.</param>
+    /// <param name="length">Maximum string length.</param>
+    /// <returns>Original string or a truncated one if the original was too long.</returns>
+    public static string Truncate(this string str, int length)
+    {
+        if (length < 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(length), "Length must be >= 0");
+        }
+
+        if (str == null)
+        {
+            return null;
+        }
+
+        int maxLength = Math.Min(str.Length, length);
+        return str.Substring(0, maxLength);
+    }
+
+    private static Random random = new Random();
+    public static string RandomString(int length, string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+    {
+        return new string(Enumerable.Repeat(chars, length)
+            .Select(s => s[random.Next(s.Length)]).ToArray());
+    }
+
 }
